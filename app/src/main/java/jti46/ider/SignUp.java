d6cb5442ca9af4500a3466d4acdf0f9e328824d0
sap1project <arie.rachmad.s@gmail.com>
2017-03-11 16:57:08 +0700

    UI gabungan splash screen, login, dashboard

--- app/src/main/java/jti46/ider/SignUp.java
+++ app/src/main/java/jti46/ider/SignUp.java
@@ -1,13 +1,28 @@
 package jti46.ider;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
+    Button buttonsigninhere;
+    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        buttonsigninhere = (Button) findViewById(R.id.signinhere);
+        mContext = getApplicationContext();
+        buttonsigninhere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(mContext, MainActivity.class);
+                startActivity(mIntent);
+            }
+        });
     }
 }
